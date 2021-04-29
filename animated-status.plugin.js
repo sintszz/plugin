1959560a498426676e9557fd0b708882cc0fb66f
sintszz <83317223+sintszz@users.noreply.github.com>
2021-04-29 19:59:14 +0200

    Update animated-status.plugin.js

--- animated-status.plugin.js
+++ animated-status.plugin.js
@@ -1,4 +1,4 @@
-//META{"name":"AnimatedStatus","source":"https://github.com/sintszz/plugin/blob/main/animated-status.plugin.js","website":""}*//
+//META{"name":"AnimatedStatus","source":"https://github.com/sintszz/plugin/blob/main/animated-status.plugin.js","website":"https://raw.githubusercontent.com/sintszz/plugin/main/animated-status.plugin.js"}*//
 
 const Editor_Type = {
 	RICH: 0,
